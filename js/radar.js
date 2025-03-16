318ed60bd344acfdaacf46dcfd1933fde2a250a9
Anna Duan <40662229+annaduan09@users.noreply.github.com>
2025-03-16 14:24:24 -0400

    remove position notes

--- js/radar.js
+++ js/radar.js
@@ -7,12 +7,12 @@ function initRadar(radarEl, categoryPercentiles) {
 
   if (categoryPercentiles.length < 4) {
     columns = [
-      ['x', '💨SPEED', '🐆AGILITY', '⚡POWER', '💪STRENGTH'],
+      ['x', 'SPEED', 'AGILITY', 'POWER', 'STRENGTH'],
       ['Position Median', 50, 50, 50, 50],
     ];
   } else {
     columns = [
-      ['x', '💨SPEED', '🐆AGILITY', '⚡POWER', '💪STRENGTH'],
+      ['x', 'SPEED', 'AGILITY', 'POWER', 'STRENGTH'],
       ['Position Median', 50, 50, 50, 50],
       ['Athlete', ...categoryPercentiles],
     ];
