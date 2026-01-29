7c1b48452d7b035842e292b67f1964c95fc7bfd9
Anna Duan <annaduan@sas.upenn.edu>
2026-01-29 15:28:22 -0500

    upped max stat values

--- js/stat_entry.js
+++ js/stat_entry.js
@@ -123,7 +123,7 @@ function initStatEntry(statListEl, positionDropdownEl, stats, positions, events)
               ${stat}
           </label>
             <div class="input-wrapper">
-              <input type="number" id="stat-entry-${stat}" name="${stat}" max="700" step="any">
+              <input type="number" id="stat-entry-${stat}" name="${stat}" max="1000" step="any">
               <span class="unit">${unit}</span>
             </div>`;
         statListItems[stat] = item;
